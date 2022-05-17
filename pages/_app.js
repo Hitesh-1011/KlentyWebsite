13e3c3b8191fe356090896842d73b11c5aaffe3b
Hitesh S V <hitesh@ghoshak.com>
2022-05-17 18:30:37 +0530

    error fix

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,7 @@ function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link
           href="https://fonts.googleapis.com/css2?family=Palette+Mosaic&family=Poppins:wght@200&family=Roboto:ital,wght@1,500&family=Rock+3D&display=swap"
           rel="stylesheet"
